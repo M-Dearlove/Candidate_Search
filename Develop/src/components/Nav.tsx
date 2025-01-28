8b989876a312e314c6e45d1bd6b319b7d314e7d3
Hydrangea <dearlove.matthew@gmail.com>
2025-01-27 19:39:59 -0500

    Nav component code.

--- Develop/src/components/Nav.tsx
+++ Develop/src/components/Nav.tsx
@@ -1,8 +1,49 @@
-const Nav = () => {
+import { CSSProperties } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+export default function Nav() {
   // TODO: Add necessary code to display the navigation bar and link between the pages
+  const location = useLocation();
+
+  const linkStyle: CSSProperties = {
+    padding: '5px',
+    color: 'white',
+    fontSize: '16pt',
+  };
+
+  const activeLinkStyle: CSSProperties = {
+    ...linkStyle,
+    color: 'coral',
+  };
+
+  const navItems = [
+    { path: '../pages/CandidateSearch', label: 'Home' },
+    { path: '../pages/SavedCandidates', label: 'Potential Candidates' }
+  ];
+
+  const sectionStyle: CSSProperties = {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'space-between',
+    padding: '1rem',
+    margin: '0 auto'
+  };
+
   return (
-    <div>Nav</div>
+    <nav>
+      <section style={sectionStyle}>
+        {navItems.map(({ path, label }) => (
+          <div key={path}>
+            <Link
+              to={path}
+              style={location.pathname === path ? activeLinkStyle : linkStyle}
+            >
+              {label}
+            </Link>
+          </div>
+        ))}
+      </section>
+    </nav>
   )
 };
-
-export default Nav;
